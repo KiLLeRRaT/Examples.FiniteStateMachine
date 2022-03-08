b10a46ac57ed19cf79d9097382f022c2318db0bd
Albert Gouws <albert@gouws.org>
2022-03-09 10:54:09 +1300

    Get program working

--- FiniteStateMachine/Document.cs
+++ FiniteStateMachine/Document.cs
@@ -1,10 +1,11 @@
+using System;
 using Stateless;
 
 namespace DocumentWorkflow.FiniteStateMachine
 {
 	public class Document
 	{
-		private enum State
+		public enum State
 		{
 			Draft,
 			Review,
@@ -14,11 +15,11 @@ namespace DocumentWorkflow.FiniteStateMachine
 			Declined
 		}
 
-		private enum Triggers
+		public enum Triggers
 		{
 			UpdateDocument,
 			BeginReview,
-			ChangedNeeded,
+			ChangesNeeded,
 			Accept,
 			Reject,
 			Submit,
@@ -27,64 +28,95 @@ namespace DocumentWorkflow.FiniteStateMachine
 			Approve,
 		}
 
-		private readonly StateMachine<State, Triggers> machine;
+		public readonly StateMachine<State, Triggers> Machine;
 
 		private readonly StateMachine<State, Triggers>.TriggerWithParameters<string> changedNeededParameters;
 
 		private async Task OnDraftEntryAsync()
 		{
+			Console.WriteLine("Document is in Draft state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal is now in the draft stage");
 		}
 
 		private async Task OnDraftExitAsync()
 		{
+			Console.WriteLine("Document is no longer in Draft state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal has now left the draft stage");
 		}
 
 		private async Task OnReviewEntryAsync()
 		{
+			Console.WriteLine("Document is in Review state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal is now in the review stage");
 		}
 
 		private async Task OnReviewExitAsync()
 		{
+			Console.WriteLine("Document is no longer in Review state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal has now left the review stage");
 		}
 
 		private async Task OnChangesRequestedEntryAsync()
 		{
+			Console.WriteLine("Document is in Changes Requested state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal is now in the changes requested stage");
 		}
 
 		private async Task OnChangesRequestedExitAsync()
 		{
+			Console.WriteLine("Document is no longer in Changes Requested state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal has now left the changes requested stage");
 		}
 
 		private async Task OnSubmittedToClientExitAsync()
 		{
+			Console.WriteLine("Document is no longer in Submitted to Client state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal has now left the submitted to client stage");
 		}
 
 		private async Task OnSubmittedToClientEnterAsync()
 		{
+			Console.WriteLine("Document is in Submitted to Client state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal is now in the approved stage");
 		}
 
 		private async Task OnDeclinedExitAsync()
 		{
+			Console.WriteLine("Document is no longer in Declined state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal has now left the declined stage");
 		}
 
 		private async Task OnDeclinedEnterAsync()
 		{
+			Console.WriteLine("Document is in Declined state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal is now in the draft stage");
 		}
 
 		private async Task OnApprovedEnter()
 		{
+			Console.WriteLine("Document is in Approved state");
 			// await notificationService.SendUpdateAsync(Priority.Verbose, "The proposal has been approved");
 		}
+
+		public async Task UpdateDocumentAsync() => await Machine.FireAsync(Triggers.UpdateDocument);
+
+		public async Task BeginReviewAsync() => await Machine.FireAsync(Triggers.BeginReview);
+
+		public async Task MakeChangeAsync(string change) => await Machine.FireAsync(changedNeededParameters, change);
+
+		public async Task AcceptAsync() => await Machine.FireAsync(Triggers.Accept);
+
+		public async Task RejectAsync() => await Machine.FireAsync(Triggers.Reject);
+
+		public async Task SubmitAsync() => await Machine.FireAsync(Triggers.Submit);
+
+		public async Task RestartReviewAsync() => await Machine.FireAsync(Triggers.RestartReview);
+
+		public async Task ApproveAsync() => await Machine.FireAsync(Triggers.Approve);
+
+		public async Task DeclineAsync() => await Machine.FireAsync(Triggers.Decline);
+
+		public State CurrentState { get { return Machine.State; } }
 
 		public Document()
 		{
@@ -92,40 +124,40 @@ namespace DocumentWorkflow.FiniteStateMachine
 			// so the constructor isn't doing long/async work.
 			//machine = new StateMachine<State, Triggers>(() => state, s => state = s);
 
-			machine = new StateMachine<State, Triggers>(State.Draft);
+			Machine = new StateMachine<State, Triggers>(State.Draft);
 
-			machine.Configure(State.Draft)
+			Machine.Configure(State.Draft)
 				.PermitReentry(Triggers.UpdateDocument)
 				.Permit(Triggers.BeginReview, State.Review)
 				.OnEntryAsync(OnDraftEntryAsync)
 				.OnExitAsync(OnDraftExitAsync);
 
-			changedNeededParameters = machine.SetTriggerParameters<string>(Triggers.ChangedNeeded);
+			changedNeededParameters = Machine.SetTriggerParameters<string>(Triggers.ChangesNeeded);
 
-			machine.Configure(State.Review)
-				.Permit(Triggers.ChangedNeeded, State.ChangesRequested)
+			Machine.Configure(State.Review)
+				.Permit(Triggers.ChangesNeeded, State.ChangesRequested)
 				.Permit(Triggers.Submit, State.SubmittedToClient)
 				.OnEntryAsync(OnReviewEntryAsync)
 				.OnExitAsync(OnReviewExitAsync);
 
-			machine.Configure(State.ChangesRequested)
+			Machine.Configure(State.ChangesRequested)
 				.Permit(Triggers.Reject, State.Review)
 				.Permit(Triggers.Accept, State.Draft)
 				.OnEntryAsync(OnChangesRequestedEntryAsync)
 				.OnExitAsync(OnChangesRequestedExitAsync);
 
-			machine.Configure(State.SubmittedToClient)
+			Machine.Configure(State.SubmittedToClient)
 				.Permit(Triggers.Approve, State.Approved)
 				.Permit(Triggers.Decline, State.Declined)
 				.OnEntryAsync(OnSubmittedToClientEnterAsync)
 				.OnExitAsync(OnSubmittedToClientExitAsync);
 
-			machine.Configure(State.Declined)
+			Machine.Configure(State.Declined)
 				.Permit(Triggers.RestartReview, State.Review)
 				.OnEntryAsync(OnDeclinedEnterAsync)
 				.OnExitAsync(OnDeclinedExitAsync);
 
-			machine.Configure(State.Approved)
+			Machine.Configure(State.Approved)
 				.OnEntryAsync(OnApprovedEnter);
 		}
 	}
--- FiniteStateMachine/Program.cs
+++ FiniteStateMachine/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DocumentWorkflow.FiniteStateMachine;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Create a new document
+Document doc = new Document();
+Console.WriteLine("Current state: " + doc.CurrentState);
+
+// Go through the states of a document based on user input
+while (true)
+{
+	if (doc.CurrentState == Document.State.Approved)
+	{
+		Console.WriteLine("Thanks for playing, bye!");
+		break;
+	}
+
+	Console.WriteLine("Available triggers: " + string.Join(", ", doc.Machine.PermittedTriggers));
+	Console.WriteLine("Enter a trigger or \"exit\": ");
+	string input = Console.ReadLine();
+	if (input == "exit")
+	{
+		break;
+	}
+
+	if (!string.IsNullOrEmpty(input))
+	{
+		if (Enum.TryParse<Document.Triggers>(input, out Document.Triggers trigger))
+		{
+		await doc.Machine.FireAsync(trigger);
+		}
+		else
+		{
+			Console.WriteLine("Invalid trigger");
+		}
+	}
+	Console.WriteLine("");
+}
+
+
+
+Console.WriteLine("Done, press any key to quit...");
+Console.ReadKey();
